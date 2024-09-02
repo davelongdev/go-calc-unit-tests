2045128f5f37bf06936d0ec5598c861bdbe1cb23
dave long <davelongdev@gmail.com>
2024-09-01 22:36:57 -0400

    add second test case

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -18,3 +18,17 @@ func TestDivide(t *testing.T) {
     t.Errorf("expected %.1f, got %.1f", expected, got)
   }
 }
+
+func TestDivideNegativeDivisor(t *testing.T) {
+
+  // what we expect the function to return
+  expected := -2.0
+
+  // what the function actualy returns
+  got := calculator.Divide(10.0, -5.0)
+
+  // what to do if got is different than expected
+  if got != expected {
+    t.Errorf("expected %.1f, got %.1f", expected, got)
+  }
+}
