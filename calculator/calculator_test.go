72f9ec85164e9171f88e9f1f8ddfe6e8f5d59e28
dave long <davelongdev@gmail.com>
2024-09-01 22:56:31 -0400

    impliment parameterized testing using test case structs

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -5,30 +5,34 @@ import (
   "github.com/davelongdev/go-calc-unit-tests/calculator"
 )
 
-func TestDivide(t *testing.T) {
 
-  // what we expect the function to return
-  expected := 2.0
-
-  // what the function actualy returns
-  got := calculator.Divide(10.0, 5.0)
-
-  // what to do if got is different than expected
-  if got != expected {
-    t.Errorf("expected %.1f, got %.1f", expected, got)
-  }
+// create struct for test cases
+var testcases = []struct {
+  name string
+  dividend float64
+  divisor float64
+  expected float64
+}{
+  // add test cases
+  {"division", 10.0, 5.0, 2.0},
+  {"division by negative value", 10.0, -5.0, -2.0},
 }
 
-func TestDivideNegativeDivisor(t *testing.T) {
+func TestDivide(t *testing.T) {
 
-  // what we expect the function to return
-  expected := -2.0
+  // loop over the test cases
+  for _, tc := range testcases {
+    t.Run(tc.name, func(t *testing.T) {
+      // what we expect the function to return
+      expected := tc.expected
 
-  // what the function actualy returns
-  got := calculator.Divide(10.0, -5.0)
+      // what the function actualy returns
+      got := calculator.Divide(tc.dividend, tc.divisor)
 
-  // what to do if got is different than expected
-  if got != expected {
-    t.Errorf("expected %.1f, got %.1f", expected, got)
+      // what to do if got is different than expected
+      if got != expected {
+        t.Errorf("expected %.1f, got %.1f", expected, got)
+      }
+    })
   }
 }
